d0ea9093679b2d076e60e79d111cd920107982a1
Philip John <phil@philipjohn.me.uk>
2021-08-16 15:42:52 +0100

    Initial commit of base plugin

---
+++ primary-category.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Primary Category
+Description: Provides a new "Primary Category" setting for individual posts and the ability to filter front-end results by that primary category.
+Requires at least: 5.8
+Author: Philip John
+Author URI: https://philipjohn.me.uk
+License: GPLv2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: primary-category
+Domain path: languages
+Update URI: https://github.com/philipjohn/primary-category
+*/
