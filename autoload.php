58719af5167f826809a69619c4fc004fe5db361f
Philip John <phil@philipjohn.me.uk>
2021-08-19 15:49:35 +0100

    Set up base plugin class and autoloader

---
+++ autoload.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Plugin Autoloader
+ *
+ * @package primary-category
+ */
+
+/**
+ * Register autoloader
+ */
+spl_autoload_register(
+	function ( $class ) {
+		// We use namespaces here.
+		$namespace = explode( '\\', $class );
+		$root      = array_shift( $namespace );
+
+		// Bail out if we're not in the namespace, even though that would be weird.
+		if ( 'PrimaryCategory' !== $root ) {
+			return;
+		}
+
+		// Grab the class name, which is the last bit.
+		$class_name = array_pop( $namespace );
+
+		// Build the file name that we expect.
+		$filename = 'class-' . $class_name . '.php';
+
+		// Whittle down to just the namespace name.
+		$namespace = trim( implode( DIRECTORY_SEPARATOR, $namespace ) );
+
+		// Deal with conventions for Classes vs files.
+		$filename  = strtolower( str_replace( '_', '-', $filename ) );
+		$namespace = strtolower( str_replace( '_', '-', $namespace ) );
+
+		// Get the plugin path.
+		$directory = dirname( __FILE__ );
+		if ( ! empty( $namespace ) ) {
+			$directory .= DIRECTORY_SEPARATOR . $namespace;
+		}
+
+		$file = $directory . DIRECTORY_SEPARATOR . $filename;
+
+		if ( file_exists( $file ) ) {
+			require $file;
+		}
+	}
+);
--- primary-category.php
+++ primary-category.php
@@ -11,3 +11,58 @@ Text Domain: primary-category
 Domain path: languages
 Update URI: https://github.com/philipjohn/primary-category
 */
+
+namespace PJ;
+
+// No naughtiness, thank you.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Require the autoloader to set everything up.
+require_once 'autoload.php';
+
+/**
+ * Plugin base class.
+ */
+class PrimaryCategory {
+
+	/**
+	 * PrimaryCategory instance.
+	 *
+	 * @var PrimaryCategory $instance
+	 */
+	private static $instance = null;
+
+	/**
+	 * Return the class instance, or create a new one.
+	 */
+	public static function get_instance() {
+
+		if ( null === self::$instance ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+
+	}
+
+	/**
+	 * Constructor.
+	 */
+	private function __construct() {
+		add_action( 'init', array( $this, 'init' ) );
+	}
+
+	/**
+	 * Fires when WordPress triggers the 'init' hook.
+	 */
+	public function init() {
+		load_plugin_textdomain(
+			'primary-category',
+			false,
+			dirname( plugin_basename( __FILE__ ) ) . '/languages'
+		);
+	}
+}
+PrimaryCategory::get_instance();
